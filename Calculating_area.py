9e8126f56991f8cf415af7e4c4293f0eb7a1da82
Andrei Buko <bukoandrey@gmail.com>
2022-04-19 11:23:48 +0200

    First programs for data input

---
+++ Calculating_area.py
@@ -0,0 +1,9 @@
+print("A program to calculate the area! Enter the two numbers you need!")
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+result = a * b
+
+print("Area =", result)
+        
---
+++ dataInput.py
@@ -0,0 +1,7 @@
+userName = input("What is your name: ")
+outPut = "Hello, " + userName + "! How are you?"
+print(outPut)
+a = int(input("Enter the number: "))
+b = int(input("Enter the number: "))
+summa = a + b
+print("Sum of numbers:", summa)
