66e9f72c21c62bd92aee4e7b6bb634de2ad019eb
Andrei Buko <bukoandrey@gmail.com>
2022-04-19 18:02:15 +0200

    Elif coming on!

--- Conditions_SpeedLimits.py
+++ Conditions_SpeedLimits.py
@@ -1,14 +1,11 @@
 speed = int(input("Car speed: "))
 if (speed > 200):
     print("This is not a plane, liar!")
+elif (speed > 60):
+    print("Driver violated the rules!")
+elif (speed == 0):
+    print("We are not going anywhere!")
+elif (speed < 0):
+    print("Negative speed doesn't exist!")
 else:
-    if (speed > 60):
-        print("Driver violated the rules!")
-    else:
-        if (speed == 0):
-            print("We are not going anywhere!")
-        else:
-            if (speed < 0):
-                print("Negative speed doesn't exist!")
-            else:
-                print("Driver is a good boy!")
+    print("Driver is a good boy!")
