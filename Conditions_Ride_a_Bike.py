21dd90f49e39729b17cfc81207b7c31f95e2fe56
Andrei Buko <bukoandrey@gmail.com>
2022-04-19 17:28:00 +0200

    First program with conditions

---
+++ Conditions_Ride_a_Bike.py
@@ -0,0 +1,7 @@
+day = int(input("Enter number of the day of the week: "))
+if (day == 7):
+    print("Today is the weekend. Going for a bike ride.")
+if (day < 1):
+    print("There is no such day.")
+if (day > 7):
+    print("There is no such day.")
