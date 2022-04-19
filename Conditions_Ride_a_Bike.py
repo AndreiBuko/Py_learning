c7c69e2a93ae7c59b58e0c75a8dd5fd89cce5e9a
Andrei Buko <bukoandrey@gmail.com>
2022-04-19 17:58:00 +0200

    Elif coming on!

--- Conditions_Ride_a_Bike.py
+++ Conditions_Ride_a_Bike.py
@@ -1,14 +1,12 @@
 day = int(input("Enter number of the day of the week: "))
 if (day == 7):
     print("Today is the weekend. Going for a bike ride.")
+elif (day < 1):
+    print("There is no such day.")
+elif (day > 7):
+    print("There is no such day.")
 else:
-    if (day < 1):
-        print("There is no such day.")
-    else:
-        if (day > 7):
-            print("There is no such day.")
-        else:
-            print("Today is a weekday.")
+    print("Today is a weekday.")
     
 
 
