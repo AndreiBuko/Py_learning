aca3fb4f0230937858a203796eff9786a37b3017
Andrei Buko <bukoandrey@gmail.com>
2022-04-20 15:32:44 +0200

    Complex conditions was added

--- Conditions_Ride_a_Bike.py
+++ Conditions_Ride_a_Bike.py
@@ -1,10 +1,8 @@
 day = int(input("Enter number of the day of the week: "))
-if (day == 7):
+if (day < 1 or day > 7):
+    print("There is no such day.")
+elif (day == 7):
     print("Today is the weekend. Going for a bike ride.")
-elif (day < 1):
-    print("There is no such day.")
-elif (day > 7):
-    print("There is no such day.")
 else:
     print("Today is a weekday.")
     
