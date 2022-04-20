f9c32624061442dcfe73eeaa061da1412e4413f7
Andrei Buko <bukoandrey@gmail.com>
2022-04-20 15:38:41 +0200

    Complex conditions was added

--- Conditions_SpeedLimits.py
+++ Conditions_SpeedLimits.py
@@ -1,8 +1,9 @@
 speed = int(input("Car speed: "))
-if (speed > 200):
-    print("This is not a plane, liar!")
-elif (speed > 60):
+
+if(speed > 60 and speed <=200):
     print("Driver violated the rules!")
+elif (speed > 200):
+    print("This is not a plane, liar!")
 elif (speed == 0):
     print("We are not going anywhere!")
 elif (speed < 0):
